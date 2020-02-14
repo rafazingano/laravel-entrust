13d8bc5a962c7824da55734d15050b235b8f68cc
rafazingano <rafazingano@gmail.com>
2020-02-14 17:06:44 -0300

    Update all

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -14,13 +14,14 @@ Route::middleware(['auth:api'])
                         Route::get('datatable', 'RoleController@datatable')->name('datatable');
                         Route::get('select2', 'RoleController@select2')->name('select2');
                     });
-
+/*
                 Route::name('permissions.')
                     ->prefix('permissions')
                     ->group(function () {
                         Route::get('datatable', 'PermissionController@datatable')->name('datatable');
                         Route::get('select2', 'PermissionController@select2')->name('select2');
                     });
+*/
 
             });
     });
