135bac69ed093a8d256603fe27b4a6d5f84bf700
Rafael Zingano <confrariaweb@gmail.com>
2020-08-20 15:34:01 -0300

    Update all

--- databases/Seeds/EntrustTableSeeder.php
+++ databases/Seeds/EntrustTableSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class EntrustTableSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
